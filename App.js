c2c9fbc2aa40e1a0ca8f6fe37327851d586992c0
Parimal Radadiya <72181399+ParimalRadadiya@users.noreply.github.com>
2022-08-31 15:19:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import Screen1 from './Components/Screen1';
+import Screen2 from './Components/Screen2';
+import Screen3 from './Components/Screen3';
+import Screen4 from './Components/Screen4';
+import Screen5 from './Components/Screen5';
+import Screen6 from './Components/Screen6';
+import Screen7 from './Components/Screen7';
+import Screen8 from './Components/Screen8';
+import Screen9 from './Components/Screen9';
+import Screen10 from './Components/Screen10';
+import Screen11 from './Components/Screen11';
+import Screen12 from './Components/Screen12';
+import Screen13 from './Components/Screen13';
+import Screen14 from './Components/Screen14';
+import Screen15 from './Components/Screen15';
+import Screen16 from './Components/Screen16';
+import Screen17 from './Components/Screen17';
+import Screen18 from './Components/Screen18';
+import Screen19 from './Components/Screen19';
+import Screen20 from './Components/Screen20';
+import Screen21 from './Components/Screen21';
+import Screen22 from './Components/Screen22';
+import Screen23 from './Components/Screen23';
+import Screen24 from './Components/Screen24';
+import Screen25 from './Components/Screen25';
+import Screen26 from './Components/Screen26';
+import Screen27 from './Components/Screen27';
+import Screen28 from './Components/Screen28';
+import Screen29 from './Components/Screen29';
+import Screen30 from './Components/Screen30';
+import Screen31 from './Components/Screen31';
+import Screen32 from './Components/Screen32';
+import Screen33 from './Components/Screen33';
+import Screen34 from './Components/Screen34';
+import Screen35 from './Components/Screen35';
+import Screen36 from './Components/Screen36';
+import Screen37 from './Components/Screen37';
+import Screen38 from './Components/Screen38';
+import Screen39 from './Components/Screen39';
+import Swipe from './Components/Swipe';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Screen1 />}></Route>
+          <Route path='/S2' element={<Screen2 />}></Route>
+          <Route path='/S3' element={<Screen3 />}></Route>
+          <Route path='/S4' element={<Screen4 />}></Route>
+          <Route path='/S5' element={<Screen5 />}></Route>
+          <Route path='/S6' element={<Screen6 />}></Route>
+          <Route path='/S7' element={<Screen7 />}></Route>
+          <Route path='/S8' element={<Screen8 />}></Route>
+          <Route path='/S9' element={<Screen9 />}></Route>
+          <Route path='/S10' element={<Screen10 />}></Route>
+          <Route path='/S11' element={<Screen11 />}></Route>
+          <Route path='/S12' element={<Screen12 />}></Route>
+          <Route path='/S13' element={<Screen13 />}></Route>
+          <Route path='/S14' element={<Screen14 />}></Route>
+          <Route path='/S15' element={<Screen15 />}></Route>
+          <Route path='/S16' element={<Screen16 />}></Route>
+          <Route path='/S17' element={<Screen17 />}></Route>
+          <Route path='/S18' element={<Screen18 />}></Route>
+          <Route path='/S19' element={<Screen19 />}></Route>
+          <Route path='/S20' element={<Screen20 />}></Route>
+          <Route path='/S21' element={<Screen21 />}></Route>
+          <Route path='/S22' element={<Screen22 />}></Route>
+          <Route path='/S23' element={<Screen23 />}></Route>
+          <Route path='/S24' element={<Screen24 />}></Route>
+          <Route path='/S25' element={<Screen25 />}></Route>
+          <Route path='/S26' element={<Screen26 />}></Route>
+          <Route path='/S27' element={<Screen27 />}></Route>
+          <Route path='/S28' element={<Screen28 />}></Route>
+          <Route path='/S29' element={<Screen29 />}></Route>
+          <Route path='/S30' element={<Screen30 />}></Route>
+          <Route path='/S31' element={<Screen31 />}></Route>
+          <Route path='/S32' element={<Screen32 />}></Route>
+          <Route path='/S33' element={<Screen33 />}></Route>
+          <Route path='/S34' element={<Screen34 />}></Route>
+          <Route path='/S35' element={<Screen35 />}></Route>
+          <Route path='/S36' element={<Screen36 />}></Route>
+          <Route path='/S37' element={<Screen37 />}></Route>
+          <Route path='/S38' element={<Screen38 />}></Route>
+          <Route path='/S39' element={<Screen39 />}></Route>
+          <Route path='/S40' element={<Swipe />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
