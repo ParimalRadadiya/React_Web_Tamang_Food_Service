c676782f5a1a5af9fbc460a94d71019f998cb581
Parimal Radadiya <72181399+ParimalRadadiya@users.noreply.github.com>
2022-08-31 15:32:48 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
