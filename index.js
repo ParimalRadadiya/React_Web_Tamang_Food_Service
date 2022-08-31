10ee944a1afc1c16f2e9bbf18b0dc83854fcb8b2
Parimal Radadiya <72181399+ParimalRadadiya@users.noreply.github.com>
2022-08-31 15:31:31 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "./App";
-import "swiper/swiper-bundle.min.css";
-import "swiper/swiper.min.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
